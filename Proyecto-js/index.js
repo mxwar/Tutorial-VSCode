86263b43bcfa0a66fee53750bab800923e3cdefd
mxwar <marco.guerra.ingeniero@gmail.com>
2022-10-15 23:21:17 -0300

    commit inicial

---
+++ Proyecto-js/index.js
@@ -0,0 +1,14 @@
+const fn = () =>{
+    const miString = "soy un string!"
+    let b = miString + " muy feliz"
+    console.log(b)
+
+}
+
+fn()
+
+const clickme = () => {
+    let a = "super feliz"
+    console.log(a)
+
+}
